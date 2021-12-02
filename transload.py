fe1c57f05334b49b258ac57db34864f9baad7d58
Sourcery AI <>
2021-12-02 15:35:00 +0000

    'Refactored by Sourcery'

--- transload.py
+++ transload.py
@@ -12,8 +12,8 @@ def trans(link):
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
                 "Accept-Encoding": "gzip, deflate",
                 "Accept-Language": "en-US,en;q=0.9" }
-                
-                
+
+
 
     data = dict(
         link = link,
@@ -40,7 +40,7 @@ def trans(link):
     #session = HTMLSession()
     #r = session.post(base,data=data,headers=headers)
     soup = bs(r.text,"lxml")
-    
+
     all_ = soup.find_all("input",type="hidden",attrs = {"name":True}, value=True)
     """method = soup.find()
     dis_plug = soup.find()
@@ -51,10 +51,8 @@ def trans(link):
     referer = soup.find()
     link = soup.find()"""
 
-    data = {}
+    data = {a["name"]: a["value"] for a in all_}
 
-    for a in all_:
-        data.update({a["name"]:a["value"]})
     #session = HTMLSession()
     #r = session.post(base,data=data,headers=h)
     j = post(base+"/index.php",data=data,headers=headers,verify=False)
@@ -66,7 +64,6 @@ def trans(link):
 
 
     #rn = r.html.render()
-
    #print(r.text)
 
     #final_link = "https://rapidleech.hashhackers.com"+d[-2]["href"]
